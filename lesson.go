830e85d4d1ae29c0f95ae8980dcb1e939c19cf44
namotoy <namotopet0615@gmail.com>
2022-06-07 18:04:04 +0900

    数値・文字列

--- lesson.go
+++ lesson.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const Pi = 3.14
+const (
+	Username = "test_user"
+	Password = "test_pass"
+)
 
 var (
 	i    int     = 1
@@ -20,8 +29,27 @@ func foo() {
 
 }
 func main() {
-
+	/*var (
+		u8  uint8     = 255
+		i8  int8      = 127
+		f32 float32   = 0.2
+		c64 complex64 = -5 + 12i
+	)
+	fmt.Println(u8, i8, f32, c64)
+	fmt.Printf("type=%T value=%v", u8, u8)
+	fmt.Println(Pi, Username, Password)
 	fmt.Println(i, f64, s, t, f)
 	foo()
+	fmt.Println("1+1=", 1+1)
+	x := 0
+	fmt.Println(x)
+	*/
+	//fmt.Println(string("Hello world"[0]))
+	var s string = "Hello world"
+	s = strings.Replace(s, "H", "X", 1)
+	fmt.Println(s)
+	fmt.Println(strings.Contains(s, "world"))
+	fmt.Println("Test\n" +
+		"Test")
 
 }
