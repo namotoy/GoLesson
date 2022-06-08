387c4959cbb4ddd45d85a6b9696ee447f21a041f
namotoy <namotopet0615@gmail.com>
2022-06-08 21:37:53 +0900

    スライス

--- lesson.go
+++ lesson.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 const Pi = 3.14
@@ -54,15 +53,55 @@ func main() {
 	//t, f := true, false
 	//fmt.Printf("%T %v %t\n ", t, t, 1)
 	//fmt.Printf("%T %v %t\n", f, f, 2)
-	var x int = 1
-	xx := float64(x)
-	fmt.Printf("%T %v %f\n", xx, xx, xx)
+	//var x int = 1
+	//xx := float64(x)
+	//fmt.Printf("%T %v %f\n", xx, xx, xx)
+	//
+	//var y float64 = 1.2
+	//yy := int(y)
+	//fmt.Printf("%T %v %d\n", yy, yy, yy)
+	//
+	//var s string = "15"
+	//i, _ := strconv.Atoi(s)
+	//fmt.Printf("%T %v", i, i)
+	//var a [2]int
+	//a[0] = 100
+	//a[1] = 200
+	//fmt.Println(a)
+	//
+	//var b [2]int = [2]int{100, 200}
+	//fmt.Println(b)
+	//n := []int{1, 2, 3, 4, 5, 6}
+	//fmt.Println(n)
+	//fmt.Println(n[2])
+	//fmt.Println(n[2:5])
+	//fmt.Println(n[:2])
+	//fmt.Println(n[2:])
+	//fmt.Println(n[:])
+	//n[2] = 100
+	//fmt.Println(n)
+	//var board = [][]int{
+	//	[]int{0, 1, 2},
+	//	[]int{3, 4, 5},
+	//	[]int{6, 7, 8},
+	//}
+	//fmt.Println(board)
+	//n = append(n, 100, 200, 300, 400)
+	//fmt.Println(n)
 
-	var y float64 = 1.2
-	yy := int(y)
-	fmt.Printf("%T %v %d\n", yy, yy, yy)
+	//n := make([]int, 3, 5)
+	//fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	//n = append(n, 0, 0)
+	//fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	//n = append(n, 1, 2, 3, 4, 5, 6)
+	var c []int
+	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+	c = make([]int, 5)
+	c = make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		c = append(c, i)
+		fmt.Println(c)
+	}
+	fmt.Println(c)
 
-	var s string = "15"
-	i, _ := strconv.Atoi(s)
-	fmt.Printf("%T %v", i, i)
 }
