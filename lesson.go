140873d166f2892c06f426c2ac6cc1fa86eb819b
namotoy <namotopet0615@gmail.com>
2022-06-10 12:11:46 +0900

    range

--- lesson.go
+++ lesson.go
@@ -58,24 +58,45 @@ func by2(num int) string {
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			fmt.Println("continue")
-			continue
-		}
+	l := []string{"python", "go", "java"}
+	for i := 0; i < len(l); i++ {
+		fmt.Println(i, l[i])
+	}
+	for i, v := range l {
+		fmt.Println(i, v)
+	}
+	for _, v := range l {
+		fmt.Println(v)
+	}
 
-		if i > 5 {
-			fmt.Println("break")
-			break
-		}
-		fmt.Println(i)
+	m := map[string]int{"apple": 100, "banana": 200}
+	for k, v := range m {
+		fmt.Println(k, v)
 	}
-	sum := 1
-	for sum < 10 {
-		sum += sum
-		fmt.Println(sum)
+	for k := range m {
+		fmt.Println(k)
 	}
-	fmt.Println(sum)
+	for _, v := range m {
+		fmt.Println(v)
+	}
+	//for i := 0; i < 10; i++ {
+	//	if i == 3 {
+	//		fmt.Println("continue")
+	//		continue
+	//	}
+	//
+	//	if i > 5 {
+	//		fmt.Println("break")
+	//		break
+	//	}
+	//	fmt.Println(i)
+	//}
+	//sum := 1
+	//for sum < 10 {
+	//	sum += sum
+	//	fmt.Println(sum)
+	//}
+	//fmt.Println(sum)
 
 	//result := by2(10)
 	//
