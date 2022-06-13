592d5d07f0b61c988bd189586f6d6e0f587d10af
namotoy <namotopet0615@gmail.com>
2022-06-13 12:08:36 +0900

    newとmakeの違い

--- lesson.go
+++ lesson.go
@@ -85,14 +85,38 @@ import (
 //	thirdPartyConnectDB()
 //}
 
-func one(x *int) {
-	*x = 1
-}
+//func one(x *int) {
+//	*x = 1
+//}
+
 func main() {
-	var n int = 100
-	one(&n)
-	fmt.Println(n)
-	fmt.Println(&*&n)
+	s := make([]int, 0)
+	fmt.Printf("%T\n", s)
+
+	m := make(map[string]int)
+	fmt.Printf("%T\n", m)
+
+	ch := make(chan int)
+	fmt.Printf("%T\n", ch)
+
+	var p *int = new(int)
+	fmt.Printf("%T\n", p)
+
+	var st = new(int)
+	fmt.Printf("%T\n", st)
+	//var p *int = new(int)
+	//fmt.Println(*p)
+	//*p++
+	//fmt.Println(*p)
+	//
+	//var p2 *int
+	//fmt.Println(p2)
+	//*p2++
+	//fmt.Println(p2)
+	//var n int = 100
+	//one(&n)
+	//fmt.Println(n)
+	//fmt.Println(&*&n)
 	//fmt.Println(n)
 	//fmt.Println(&n)
 	//var p *int = &n
